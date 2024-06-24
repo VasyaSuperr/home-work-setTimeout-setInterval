11f6f8f9355ecb6136d27b51e69b3a80c2dc8950
VasyaSuperr <yenakii.vasyl@gmail.com>
2024-06-24 17:39:58 +0300

    Task 1 (use setTimeout)

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,20 +13,34 @@ function checkNumber(value, nameParam) {
   }
 }
 
+// function printNumbers(from, to, interval) {
+//   checkIntNumber(from, "from");
+//   checkIntNumber(to, "to");
+//   checkNumber(interval, "interval");
+
+//   let timerId = setInterval(() => {
+//     if (to - from === 0) {
+//       clearInterval(timerId);
+//     }
+//     console.log(from++);
+//   }, interval);
+// }
+
+// printNumbers(10, 15, 1000);
+
+// Task 2 (use setTimeout)
 function printNumbers(from, to, interval) {
   checkIntNumber(from, "from");
   checkIntNumber(to, "to");
   checkNumber(interval, "interval");
 
-  let timerId = setInterval(() => {
-    if (to - from === 0) {
-      clearInterval(timerId);
-    }
+  setTimeout(function tick() {
     console.log(from++);
-  }, interval);
+
+    if (from <= to) {
+      setTimeout(tick, interval);
+    }
+  });
 }
 
 printNumbers(10, 15, 1000);
-
-// Task 2 (use setTimeout)
-// function printNumbers(from, to, interval) {}
